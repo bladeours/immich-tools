0d4c80528be3f1e090f5fe3b272e52523a0d7253
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2025-04-01 17:05:54 +0000

    add merging xmp and running job

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -22,3 +22,12 @@ def send_post(path: str, url:str, api_key:str, data: dict={}):
     log.error(f"got error for request {url + path}: {err}")
     sys.exit(-1)
   return response
+
+def send_put(path: str, url:str, api_key:str, data: dict={}):
+  try:
+    response = r.put(url + path, json=data, headers={"x-api-key": api_key})
+    response.raise_for_status()
+  except r.exceptions.HTTPError as err:
+    log.error(f"got error for request {url + path}: {err}")
+    sys.exit(-1)
+  return response
