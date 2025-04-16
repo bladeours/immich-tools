6718977169a87a47ffe517d59b412d0bedd9318d
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2025-04-16 09:19:49 +0000

    fix: formatting

--- src/commands/run_job.py
+++ src/commands/run_job.py
@@ -4,16 +4,16 @@ import logging
 
 log = logging.getLogger("immich-tools")
 
+
 @click.command()
-@click.option('-k', '--api-key', required=True, envvar="IMMICH_API_KEY")
-@click.option('-u', '--url', required=True, envvar="IMMICH_URL")
+@click.option("-k", "--api-key", required=True, envvar="IMMICH_API_KEY")
+@click.option("-u", "--url", required=True, envvar="IMMICH_URL")
 @click.argument("job_name")
 def run_job(api_key, url, job_name):
     """Runs job, possible values: thumbnailGeneration, metadataExtraction,
     videoConversion, faceDetection, facialRecognition, smartSearch, duplicateDetection,
     backgroundTask, storageTemplateMigration, migration, search, sidecar, library, notifications, backupDatabase"""
     log.debug(f"immich url: {url}")
-    send_put(path=f"/api/jobs/{job_name}", url=url, api_key=api_key, data={"command":"start", "force": True}).json()
+    send_put(path=f"/api/jobs/{job_name}", url=url, api_key=api_key, data={"command": "start", "force": True}).json()
     log.debug(f"running job: {job_name}")
     log.info("success")
-
