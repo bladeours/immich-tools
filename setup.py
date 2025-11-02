2772dfb266214b936dbabccd1f3087dab9910c10
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2025-11-02 19:23:26 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="immich-tools",
-    version="0.2.9",
+    version="0.2.10",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["click", "requests", "PyExifTool", "pytest", "dacite"],
     entry_points={
         "console_scripts": [
-            "immich-tools = src.cli:main",
+            "immich-tools = cli:main",
         ],
     },
 )
