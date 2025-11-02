ff20cf93daf04a5b1d56f92a96bc190fe795390d
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2025-11-02 18:52:02 +0100

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="immich-tools",
-    version="0.1.8",
+    version="0.2.6",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["click", "requests", "PyExifTool", "pytest", "dacite"],
